1ed9d2622aebaaead741d7bc35a8ec7baef48d5e
Rob Weaver <rob@accuweaver.com>
2013-03-03 14:51:01 -0800

    Added TourViewController and test images + dictionary
    Nothing really working in this code yet, just placeholder for tour that
    will segue from Navigation on startup.

---
+++ MotorCade/TourViewController.h
@@ -0,0 +1,15 @@
+//
+//  TourViewController.h
+//  MotorCade
+//
+//  Created by Rob Weaver on 3/3/13.
+//  Copyright (c) 2013 Griffin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TourViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIScrollView *tourScrollView;
+@property (weak, nonatomic) IBOutlet UIPageControl *tourPageControl;
+
+@end
---
+++ MotorCade/TourViewController.m
@@ -0,0 +1,39 @@
+//
+//  TourViewController.m
+//  MotorCade
+//
+//  Created by Rob Weaver on 3/3/13.
+//  Copyright (c) 2013 Griffin. All rights reserved.
+//
+
+#import "TourViewController.h"
+
+@interface TourViewController ()
+
+@end
+
+@implementation TourViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    self.title = @"Tutorial";
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
