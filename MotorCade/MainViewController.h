25df9ed38f9cd302a5faa5d25d55bcba9054cbc7
Rob Weaver <rob@accuweaver.com>
2013-03-04 13:50:44 -0800

    Storyboard changes from Grifin

---
+++ MotorCade/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  MotorCade
+//
+//  Created by Rob Weaver on 3/4/13.
+//  Copyright (c) 2013 Griffin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
